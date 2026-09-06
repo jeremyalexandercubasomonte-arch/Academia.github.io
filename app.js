9994f628e848cbf5d4a6da6649ebe575f149c979
jeremyalexandercubasomonte-arch <jeremyalexandercubasomonte@gmail.com>
2026-09-06 01:52:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ class App {
             if (user) {
                 this.currentUser = user;
                 const profile = await getUserProfile(user.uid);
-                this.isAdmin = (profile && profile.role === "admin") || user.email === "lagloriosacervecera.91@gmail.com";
+                this.isAdmin = (profile && profile.role === "admin") || user.email === "jeremyalexandercubasomonte@gmail.com";
                 
                 // Cargar favoritos del usuario de Firebase
                 this.favorites = await getUserFavorites(user.uid);
